2b3f60c148971d97ac150c5c9a5d99e82dcb4ebb
Mars <marshallmiller92@gmail.com>
2016-02-04 14:09:55 -0800

    dumb JS refresher

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 // alert("Fuck You")
+function constructThis(name, whoCares, whereAmIGoing) {
+  this.name = name;
+  this.whoCares = whoCares;
+  this.whereAmIGoing = whereAmIGoing;
+};
+var allNames = [new constructThis("Mars", "Nobody", "Marshall\'s House"),
+                new constructThis("Marshall", "Nobody", "Already here")];
+
+
+
+var easterEgg = document.getElementById('easterEgg');
+easterEgg.addEventListener('click', alertFunction);
+
+
+function alertFunction() {
+  alert('Fuck you!')
+}
